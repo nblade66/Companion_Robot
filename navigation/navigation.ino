c32625ef4e48e90e38579adcdcdf8e4542883ba5
Nathan Hsu <thequicks2001@gmail.com>
2021-01-21 01:09:34 -0500

    Adds semicolon

--- navigation/navigation.ino
+++ navigation/navigation.ino
@@ -295,7 +295,7 @@ int go_distance(byte unitDistance, Directions dir) {
   byte unitsTraveled = (dir == forward) ? distanceTicks / (cm2ticks * precision) : distanceTicks / (deg2ticks * units2deg);
   byte msg = generateDistSerial(obstacle, unitsTraveled);
 
-  Serial.write(msg)
+  Serial.write(msg);
 
   return unitsTraveled;
 
